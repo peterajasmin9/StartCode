c8c8fed81af6a21931a6b208e0fee34e9be6ad03
peterajasmin9 <peter.a.jasmin@live.com>
2020-03-22 11:40:16 -0400

    Update

--- Powershell/NetAdapters.ps1
+++ Powershell/NetAdapters.ps1
@@ -3,10 +3,26 @@ $adapters = Get-netadapter
 foreach ($adapter in $adapters)
 {
     $Name = $adapter.Name
+    
     Write-Host "Creating file for $Name."
     Start-Sleep 1
-    $adapter | format-list * | Out-File "C:\Data\Adapter-$Name.txt" -Force
+    
+    New-Item -itemtype file -path "C:\Data\Adapter-$Name.txt" -Force
     Start-Sleep 1
+
+    $adapter | format-list * | Add-Content "C:\Data\Adapter-$Name.txt" -Append
+    Start-Sleep 1
+    
+    $IPinfo = " "
+    $IPinfo = Get-NetIPAddress -interfacealias $Name
+
+    Add-Content -Path "C:\Data\Adapter-$Name.txt" -Value " "
+    Add-Content -Path "C:\Data\Adapter-$Name.txt" -Value "IP information"
+    Add-Content -Path "C:\Data\Adapter-$Name.txt" -Value " "
+
+    $IPinfo | Out-File -FilePath "C:\Data\Adapter-$Name.txt" -Append
+    Start-Sleep 1
+    
     Start-Process -FilePath notepad.exe -ArgumentList "C:\Data\Adapter-$Name.txt"
-    Start-Sleep 1
+
 }
