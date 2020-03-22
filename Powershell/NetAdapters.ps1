ce4cc90ffdcff8fc3650d99bf93d842cf87cb5f5
peterajasmin9 <peter.a.jasmin@live.com>
2020-03-22 11:19:22 -0400

    update

---
+++ Powershell/NetAdapters.ps1
@@ -0,0 +1,12 @@
+$adapters = Get-netadapter
+
+foreach ($adapter in $adapters)
+{
+    $Name = $adapter.Name
+    Write-Host "Creating file for $Name."
+    Start-Sleep 1
+    $adapter | format-list | Out-File "C:\Data\Adapter-$Name.txt" -Force
+    Start-Sleep 1
+    Start-Process -FilePath notepad.exe -ArgumentList "C:\Data\Adapter-$Name.txt"
+    Start-Sleep 1
+}
