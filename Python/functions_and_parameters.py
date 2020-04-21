41a54da722f7b65e9eece533a4aba452d6f38fd2
peterajasmin9 <peter.a.jasmin@live.com>
2020-04-20 21:45:54 -0400

    commit

---
+++ Python/functions_and_parameters.py
@@ -0,0 +1,13 @@
+def f(x=2):
+    return x**x
+print(f())
+print(f(4))
+
+# exponents x**x
+
+
+
+def add_it(x, y=10):
+    return x + y
+result = add_it(2)
+print(result)
---
+++ Python/functions_builtin.py
@@ -0,0 +1,29 @@
+# built-in functions in python
+
+# return length
+one = len("Monty")
+
+print(one)
+
+# return string
+str("100")
+
+
+# return integer
+int("1")
+
+# return float
+float(100)
+
+
+int("110")
+int(20.54)
+float("16.4")
+float(99)
+
+age = input("Enter your age:")
+int_age = int(age)
+if int_age < 21:
+    print("You are young!")
+else:
+    print("Wow, you are old!")
